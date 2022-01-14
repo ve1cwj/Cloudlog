8e96c24be8f4698daa47326e4e30f714e2ffa04c
ve1cwj <67641637+ve1cwj@users.noreply.github.com>
2022-01-14 07:22:36 -0400

    Add Canadian provinces to Create Station Location state dropdown
    Currently these are available when editing a station location, but not while creating a new location

--- application/views/station_profile/create.php
+++ application/views/station_profile/create.php
@@ -115,6 +115,19 @@
 				<option value="WI">Wisconsin</option>
 				<option value="WV">West Virginia</option>
 				<option value="WY">Wyoming</option>
+				<option value="AB">Alberta</option>
+				<option value="BC">British Columbia</option>
+				<option value="MB">Manitoba</option>
+				<option value="NB">New Brunswick</option>
+				<option value="NL">Newfoundland & Labrador</option>
+				<option value="NS">Nova Scotia</option>
+				<option value="NT">Northwest Territories</option>
+				<option value="NU">Nunavut</option>
+				<option value="ON">Ontario</option>
+				<option value="PE">Prince Edward Island</option>
+				<option value="QC">Quebec</option>
+				<option value="SK">Saskatchewan</option>
+				<option value="YT">Yukon</option>
 			</select>
 		    <small id="StateHelp" class="form-text text-muted">Station state. Applies to certain countries only. Leave blank if not applicable.</small>
 		  </div>
